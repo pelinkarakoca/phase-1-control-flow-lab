432be2744cd1cdb07ffc085b26babb38cf326ff0
Pelin Karakoca <karakocapelin@gmail.com>
2022-10-07 13:35:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400){
+    return 'This one is on me!';
+  }
+  else if (someValue > 400 && someValue <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go.";
+  }
 }
-
-function switchOnCharmFromTip(){
+let getTip;
+function switchOnCharmFromTip(getTip){
   // Write your code here!
+  switch (getTip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+        return "Thank you.";
+        break;
+    default :
+      return "Bye.";
+      break;
+  }
 }
